85f7ad8cb0b825f5550963e544f885df12dd6960
Ridam jain <47075953+ridam258@users.noreply.github.com>
2022-10-28 18:42:38 +0530

    Create elevator_rides.cpp

---
+++ CSES-SOLUTIONS/Dynamic-Programming/Elevator Rides/elevator_rides.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define ff              first
+#define ss              second
+#define int             long long
+#define uniq(v)         (v).erase(unique(all(v)),(v).end())
+#define pb              push_back
+#define all(v)          v.begin(),v.end()
+#define pii             pair<int,int>
+#define mem1(a)         memset(a,-1,sizeof(a))
+#define mem0(a)         memset(a,0,sizeof(a))
+#define vi              vector<int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define endl			"\n"
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define rep(i,a,b)      for(int i=a;i<b;i++)
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+ 
+template<typename T, typename T1>T amax(T &a, T1 b) {if (b > a)a = b; return a;}
+template<typename T, typename T1>T amin(T &a, T1 b) {if (b < a)a = b; return a;}
+template<typename T>
+using pbds = tree<T , null_type , less<T> , rb_tree_tag , tree_order_statistics_node_update>;
+const int N = 1e5 + 5;
+ 
+void solve() {
+	int n, x; cin >> n >> x;
+	vi v(n);
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+ 
+	vector<pii>dp(1 << n);
+	// dp[mask] = {minimum no. of rides, minimum weight of last ride}
+ 
+	dp[0] = {0, inf};
+	for (int mask = 1; mask < (1 << n); mask++) {
+		pii t = {inf, inf};
+		for (int j = 0; j < n; j++) {
+			if ((1ll << j) & (mask)) {
+				auto z = dp[mask ^ (1ll << j)];
+				if (z.second + v[j] <= x) {
+					z.second += v[j];
+				}
+				else {
+					z.first++;
+					z.second = min(z.second, v[j]);
+				}
+				t = min(t, z);
+			}
+		}
+		// cout << mask << " " << t.ff << " " << t.ss << endl;
+		dp[mask] = t;
+ 
+ 
+	}
+	int z_Mask = (1 << n) - 1;
+	cout << dp[z_Mask].ff << endl;
+ 
+ 
+}
+ 
+ 
+int32_t main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+#ifdef SIEVE
+	sieve();
+#endif
+#ifdef NCR
+	init();
+#endif
+	int t = 1;
+	// cin >> t;
+	while (t--) solve();
+	return 0;
+ 
+}
